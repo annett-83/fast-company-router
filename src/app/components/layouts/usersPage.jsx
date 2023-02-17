3e030eb638afaf35967874fffc6118a13fe533db
nova8 <aaa.auksutat@gmail.com>
2023-02-17 20:26:57 +0100

    after a day of honest wor

--- src/app/components/layouts/usersPage.jsx
+++ src/app/components/layouts/usersPage.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-
+import api from "../../api";
+import QualitiesList from "../qualitiesList";
+import { useHistory } from "react-router-dom";
 const UserPage = ({ id }) => {
-    return <div >{id} </div>;
+    const history = useHistory();
+    const [user, setUser] = useState();
+    useEffect(() => {
+        api.users.getById(id).then((data) => setUser(data));
+    });
+    const handeleBackToUser = () => {
+        history.replace("/users");
+    };
+    if (user) {
+        return (
+            <div>
+                <div>{user.name}</div>
+                <div>Профессия: {user.profession.name}</div>
+                <QualitiesList qualities={user.qualities} />
+                <div>completedMeetings: {user.completedMeetings}</div>
+                <div>Rate: {user.rate}</div>
+                <button
+                    onClick={() => {
+                        handeleBackToUser();
+                    }}
+                >
+                    Все пользователи
+                </button>
+            </div>
+        );
+    }
+    return "loading...";
 };
 
 UserPage.propTypes = {
